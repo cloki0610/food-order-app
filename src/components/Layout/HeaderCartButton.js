cfe909716a6f749b41c3bfd6a5b1c03702358111
Rocky.C <cloki0610@gmail.com>
2022-04-07 16:50:24 +0000

    Apply context to Header Cart component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
+import { useContext } from "react";
+import CartContext from "../../store/CartContext";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./CartIcon";
 
-const HeaderCartButton = props => {
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+const HeaderCartButton = (props) => {
+  const cartCxt = useContext(CartContext);
+  const numberOfCartItems = cartCxt.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
-export default HeaderCartButton
+export default HeaderCartButton;
