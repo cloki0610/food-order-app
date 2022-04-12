0b8baf34ade756e2d6079218cf365d60467468fc
Rocky.C <cloki0610@gmail.com>
2022-04-12 02:34:19 +0000

    Handle request error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,11 +6,17 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch("./meals.json");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = response.json();
 
       const loadedMeals = [];
@@ -24,15 +30,29 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
-    setIsLoading(false);
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
-    return <section className={classes.MealsLoading}>
-    <p>Loading...</p>
-    </section>
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => (
